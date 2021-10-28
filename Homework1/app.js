afc01b6c1fed7b79bffb489d01519781c96cc4c2
4lbatr0s <serhatoner@protonmail.com>
2021-10-28 18:26:32 +0300

    first node js homework from patika.dev

---
+++ Homework1/app.js
@@ -0,0 +1,7 @@
+console.log('How are you?')
+console.log(`I'm a robot`)
+console.log('Nice to meet you.')
+
+setTimeout(()=> {console.log(`I'm a robot`)}, 1000)
+setTimeout(()=> {console.log(`Nice too meet you`)}, 2000)
+setTimeout(()=> {console.log(`How are you?`)}, 2500)
