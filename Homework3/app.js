5c17dffecc9b9a1667579ee8f9336a47c07c5a3c
4lbatr0s <serhatoner@protonmail.com>
2021-10-28 18:51:33 +0300

    patika.dev module homework

---
+++ Homework3/app.js
@@ -0,0 +1,3 @@
+import {circleArea, circleCircumference} from './circle.js'
+circleArea(5)
+circleCircumference(5)
---
+++ Homework3/circle.js
@@ -0,0 +1,5 @@
+
+const circleArea = (r)=> console.log(3.14 * Math.pow(r,2))
+const circleCircumference = (r) => console.log(2*3.14*r);
+
+export default {circleArea, circleCircumference}
